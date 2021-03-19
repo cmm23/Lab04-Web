3c6ccd693365b641f48641014994c087d48d2dba
cmm23 <56312720+cmm23@users.noreply.github.com>
2021-03-19 11:37:49 -0600

    Crear script.js

---
+++ script.js
@@ -0,0 +1,47 @@
+// #1 Variables
+let post = document.getElementById("ButtonPost");
+let clear = document.getElementById("ButtonClear");
+let mark = document.getElementById("ButtonMark");
+let del = document.getElementById("ButtonDelete");
+
+// #2 Listeners
+post.addEventListener("click",TodoPost);
+clear.addEventListener("click", TodoClear);
+mark.addEventListener("click", TodoMark);
+del.addEventListener("click", TodoDel);
+
+// #3 Funciones
+
+function TodoPost(e){
+  e.preventDefault();
+  //Obtener el valor del TextArea
+  var todo = document.getElementById("todoText").value;
+  var list = document.getElementById("todoList");
+
+  var currentListHTML = list.innerHTML;
+  if(todo != ""){
+    var currListHTML = list.innerHTML;
+    list.innerHTML = currListHTML + `<input type = "checkbox" name = "todo" / > ${todo} <br>`
+  }
+}
+
+function TodoClear(e){
+  var todos = document.getElementsByName("todo");
+
+  for(var i=0; i<todos.length; i++){
+    todos[i].checked = false;
+  }
+}
+
+function TodoDel(e){
+  var list = document.getElementById("todoList");
+  list.innerHTML = "";
+}
+
+function TodoMark(e){
+  var todos = document.getElementsByName("todo");
+
+  for(var i=0; i<todos.length; i++){
+    todos[i].checked = true;
+  }
+}
